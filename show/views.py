0d37484db03ac67a28482813363169121b822095
Tanuja Joshi <joshitanuja276@gmail.com>
2020-10-10 20:53:09 +0530

    added views and urls

--- show/views.py
+++ show/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+
+from django.contrib.auth import login,logout,authenticate
+
+
+def index(request):
+	if request.user.is_authenticated:
+		return render(request,"show/dashboard.html")
+
+	else:
+		return render(request,"show/home.html")
+
+
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, email=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "show/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "show/login.html")
+
+
+
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+
+
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email=request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "show/register.html", {
+                "message": "Passwords must match."
+            })
+
+      
+        # try:
+        #     user = User.objects.create_user(username, email, password)
+        #     user.save()
+        # except IntegrityError as e:
+        #     print(e)
+        #     return render(request, "mail/register.html", {
+        #         "message": "Email address already taken."
+        #     })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "show/register.html")
